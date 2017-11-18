0fa092e81adf1dec6fa426f258f03791754b1cba
seinnaidess <contactseinna@gmail.com>
2017-11-17 20:38:51 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Rubber Ducky Bot for Doorsteps Devs to work their issues out with.
+
+
---
+++ src/server.mjs
@@ -0,0 +1,36 @@
+const express = require('express') //node 8.5 native module support does not support libraries.
+const debug = require('debug')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+
+const app = express()
+const log = debug('server')
+
+const port = process.env.PORT || 7070
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(logger('tiny'))
+
+
+app.listen(port, () => {
+  console.log('We are live on ' + port)
+  console.log('Let the games begin...')
+})
+
+app.get('/', (req, res) => {
+  res.status(200).send('Tha duck don\'t cluck! Stay ready.')
+})
+
+app.post('/duck', (req, res) => {
+  const name = req.body.user_name
+  const payload = {
+    text: `...tha duck is here for you, ${name}.`
+  }
+
+  return name !== 'slackbot' ? res.status(200).json(payload) : res.status(200).end()
+})
+
+
+
+
+
